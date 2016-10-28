0ebea8e07dddc606a6fe45b4f2fec90b58481723
gdmec07150827 <1140681663@qq.com>
2016-10-28 16:23:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asus/intent/IntentDemo3.java
@@ -0,0 +1,17 @@
+package com.example.asus.intent;
+
+/**
+ * Created by ASUS on 2016/10/28.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntentDemo3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
